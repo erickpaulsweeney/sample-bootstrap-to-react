beb35860c46071483e8922e7fb408fb1e566c0c9
ErickPaulSweeney <erickpaulsweeney@gmail.com>
2022-10-14 16:19:08 +0800

    Done

---
+++ src/components/App.jsx
@@ -0,0 +1,23 @@
+import About from "./About";
+import Contact from "./Contact";
+import Footer from "./Footer";
+import Masthead from "./Masthead";
+import NavBar from "./NavBar";
+import Projects from "./Projects";
+import Signup from "./Signup";
+
+function App() {
+    return (
+        <>
+            <NavBar />
+            <Masthead />
+            <About />
+            <Projects />
+            <Signup />
+            <Contact />
+            <Footer />
+        </>
+    );
+}
+
+export default App;
